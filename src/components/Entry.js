584f431434cac2a9efbdc0b391606fe97e51fec7
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-03-17 16:42:19 +0530

    update

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -33,26 +33,90 @@ const Entry = () => {
                     </div>
      
                 </div>
+
+
                 <div className='quantity-box'>
+                    <div className='quantity-box-top'>
+                        <div className='quantity-box-top-header'>
+                            <p>S.No.</p>
+                            <p>Product Name</p>
+                            <p>Unit Pack</p>
+                            <p>Quantity</p>
+                            <p>Free</p>
+                            <p>Trade Rate</p>
+                            <p>Disc%</p>
+                            <p>Amt</p>
+                        </div>
+                        <div className='quantity-box-top-content'>
+                            <p>1</p>
+                            <p>Shwetank </p>
+                            <p>1</p>
+                            <p>100</p>
+                            <p>56</p>
+                            <p>19</p>
+                            <p>15</p>
+                            <p>99</p>
+                        </div>
+                        <div className='quantity-box-top-content'>
+                            <p>1</p>
+                            <p>Shwetank </p>
+                            <p>1</p>
+                            <p>100</p>
+                            <p>56</p>
+                            <p>19</p>
+                            <p>15</p>
+                            <p>99</p>
+                        </div>
+                        <div className='quantity-box-top-content'>
+                            <p>1</p>
+                            <p>Shwetank </p>
+                            <p>1</p>
+                            <p>100</p>
+                            <p>56</p>
+                            <p>19</p>
+                            <p>15</p>
+                            <p>99</p>
+                        </div>
+                    </div>
+                    <div className='quantity-box-bottom'>
+                        <div className='qty-comments'>
+
+                        </div>
+
+                        <div className='qty-details'>
+                                <div className='quantity-box-top-header'>
+                                <p>Date </p>
+                                <p>Invoice No.</p>
+                                <p>Amt.</p>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
             <div className='contact-box'>
                 <div className='contact-box-input'>
                     <label htmlFor=''>Address</label><br />
-                    <input id='' /><br/>
-                    <label htmlFor=''>Address</label><br />
-                    <input id='' /><br/>
-                    <label htmlFor=''>Address</label><br />
-                    <input id='' /><br/>
-                    <label htmlFor=''>Address</label><br />
-                    <input id='' /><br/>
-                    <label htmlFor=''>Address</label><br />
-                    <input id='' /><br/>
-                    <label htmlFor=''>Address</label><br />
-                    <input id='' /><br/>
-                    <label htmlFor=''>Address</label><br />
-                    <input id='' /><br/>
+                    <input id='' /><br/><br />
+
+                    <label htmlFor=''>Email</label><br />
+                    <input id='' /><br/><br />
+
+                    <label htmlFor=''>Mobile No.</label><br />
+                    <input id='' /><br/><br />
+
+                    <label htmlFor=''>GSTIN</label><br />
+                    <input id='' /><br/><br />
+
+                    <label htmlFor=''>D.L.1</label><br />
+                    <input id='' /><br/><br />
+
+                    <label htmlFor=''>D.L.2</label><br />
+                    <input id='' /><br/><br />
+
+                    <label htmlFor=''>FSSAI No.</label><br />
+                    <input id='' /><br/><br />
+                    
                 </div>
 
             </div>
@@ -60,6 +124,21 @@ const Entry = () => {
 
         <div className='bottom-half'>
             <div className='total-box'>
+                <div className='total-empty'>
+                    <p>comments</p>
+                </div>
+
+                <div className='total-values'>
+                    <div className='total-values-button'>
+                        <button>Sub. Total</button>
+                        <button>Disc. Amount</button>
+                        <button>Total Tax</button>
+                        <button>Goods Return(R)</button>
+                        <button>CN. Voucher </button>
+                        <button>Grand Total</button>
+                    </div>
+
+                </div>
                 
             </div>
             <div className='blank-box'>
---
+++ src/components/Purchase.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./styles.css"
+
+const Purchase = () => {
+  return (
+    <div className='main'>
+
+        <h1>Grid Layout</h1>
+
+            <p>This grid layout contains six columns and three rows:</p>
+
+            <div class="wrapper">
+                <div class="box item1">One</div>
+                <div class="box item2">Two</div>
+                <div class="box item3">Three</div>
+                <div class="box item4">Four</div>
+                <div class="box item5">Five</div>
+            </div>      
+
+    </div>
+  )
+}
+
+export default Purchase
